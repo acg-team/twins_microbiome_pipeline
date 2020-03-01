66625ffdcf7990a8fe5257629249ce1bc9e4fd22
Alex Y <github.alex.ai@gmail.com>
2020-03-01 18:04:03 +0300

    anova

--- src/analysis_twin/ANOVA.R
+++ src/analysis_twin/ANOVA.R
@@ -1,7 +1,13 @@
 # This is a fule for AKOS to modify
 # https://github.com/joey711/phyloseq/wiki/ordinate
+# http://joey711.github.io/phyloseq/plot_ordination-examples
+# https://github.com/joey711/phyloseq/wiki/ordinate
 
 library(ggplot2)
+library(dplyr)
+library("ggpubr")
+library(lubridate)
+
 source("src/configure.R")
 source("src/load.R")
 my_path  <- file.path(project_path, "src/analysis_twin")
@@ -28,6 +34,8 @@ ps.tweens.log <- transform_sample_counts(ps.tweens.norm, function(x) log(1 + x))
 
 ###############  study within family sample variability   ########
 twin.families <- unique(df.metadata.4timepoints$family_id)
+twin.families.mz <- unique(df.metadata.4timepoints[df.metadata.4timepoints$zygosity=="MZ",]$family_id)
+twin.families.dz <- unique(df.metadata.4timepoints[df.metadata.4timepoints$zygosity=="DZ",]$family_id)
 
 print_family <- function(family_id){
   print(df.metadata.4timepoints[df.metadata.4timepoints$family_id==twin.families[family.number], ])
@@ -39,18 +47,18 @@ get_year <- function(date_factor){
 }
 
 # initialize matrix for intra / extra  distances
-df.intr.extr<-data.frame("same_twin_diff_time","dif_twins_same_time","diff_twins_diff_time")
-
-
-library(lubridate)
-
-# CHANGE THE SOURCE PS FILI HERE 
-ps <- ps.tweens.norm   # ps.tweens.norm  / ps.tweens.log
+df.intr.extr <- data.frame("same_twin_diff_time","dif_twins_same_time","diff_twins_diff_time")
 same.twin.different.years <- c()
 same.year.different.twins <- c()
 diff.year.different.twins <- c()
 
-for (family.number in twin.families){
+
+# CHANGE THE SOURCE HERE 
+ps <- ps.tweens.norm   # ps.tweens.norm  / ps.tweens.log
+
+
+# go throught every family ans stack distances to same.twin.different.years, etc
+for (family.number in twin.families.dz){
   print(family.number)
   twin.family.samples <- df.metadata.4timepoints[df.metadata.4timepoints$family_id==family.number, ]$file
   ps.onefamily <- phyloseq::subset_samples(ps, (sample_names(ps) %in% twin.family.samples))
@@ -136,6 +144,8 @@ for (family.number in twin.families){
   
   
 }# end main loop
+
+
 same.twin.different.years <- na.omit(same.twin.different.years)
 same.year.different.twins <- na.omit(same.year.different.twins)
 diff.year.different.twins <- na.omit(diff.year.different.twins)
@@ -145,12 +155,51 @@ print(paste("Twins variability - Same year different twins (mean/std)", mean(sam
 print(paste("Cross - Different year different twins (mean/std)", mean(diff.year.different.twins), sd(diff.year.different.twins)))
 
 
+########################### prepare dataframe to ANOVA
+df1 <- data.frame(same.twin.different.years, "same.twin.different.years" )
+colnames(df1) <- c("distance","group")
 
-## check ordinate(ps.onefamily, "NMDS", "unifrac")
-## filter out if only one example
+df2 <- data.frame(same.year.different.twins, "same.year.different.twins" )
+colnames(df2) <- c("distance","group")
 
-# http://joey711.github.io/phyloseq/plot_ordination-examples
-# https://github.com/joey711/phyloseq/wiki/ordinate
+df3 <- data.frame(diff.year.different.twins, "diff.year.different.twins" )
+colnames(df3) <- c("distance","group")
+
+df.anova <- rbind(df1, df2, df3)
+
+
+# general statistics
+group_by(df.anova, group) %>%
+  summarise(
+    count = n(),
+    mean = mean(distance, na.rm = TRUE),
+    sd = sd(distance, na.rm = TRUE)
+  )
+
+
+ggboxplot(df.anova, x = "group", y = "distance", 
+          color = "group", palette = c("#00AFBB", "#E7B800", "#FC4E07"),
+          order = c("same.twin.different.years", "same.year.different.twins", "diff.year.different.twins"),
+          ylab = "distances", xlab = "groups")
+
+
+# Compute the analysis of variance
+# F-statistic which can be expressed as the ratio of Between Group variability and Within Group Variability.
+# F = Between group variability / Within group variability
+#   In other words, the null hypothesis states that all the sample means are equal or 
+#   the factor did not have any significant effect on the results. 
+#   Whereas, the alternate hypothesis states that at least one of the sample means is different from another. 
+#   But we still can’t tell which one specifically. For that, we will use other methods 
+res.aov <- aov(distance ~ group, data = df.anova)
+# Summary of the analysis
+summary(res.aov)
+
+
+
+# 1. Homogeneity of variances
+plot(res.aov, 1)
+# 2. Normality
+plot(res.aov, 2)
 
 
 
