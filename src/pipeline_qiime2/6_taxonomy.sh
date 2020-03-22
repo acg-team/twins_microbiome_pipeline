fd0b8bd7bfa659e4406bd1da7b62c6b1fcb33818
Alex Y <github.alex.ai@gmail.com>
2020-03-22 18:14:11 +0300

    phyloseq forming

--- src/pipeline_qiime2/6_taxonomy.sh
+++ src/pipeline_qiime2/6_taxonomy.sh
@@ -1,6 +1,6 @@
 ## Taxonomic analysis using greengenes
 P=~/Projects_R/twins_microbiome_pipeline/data_set_twin/raw/qiime/
-GG=~/Projects_R/twins_microbiome_pipeline/green_genes/
+GG=~/Projects_R/twins_microbiome_pipeline/tax_green_genes/
 OUT=${P}taxonomy/
 
 
@@ -8,41 +8,42 @@ OUT=${P}taxonomy/
 ### First download the greengenes reference database on QIIME2 ###(https://docs.qiime2.org/2019.10/data-resources/)
 ### and import the otus file as a qiime2 object
 # https://docs.qiime2.org/2020.2/data-resources/
-qiime tools import 
-    --type "FeatureData[Sequence]" 
-    --input-path ${GG}gg_13_8_otus/rep_set/99_otus.fasta 
+qiime tools import \
+    --type "FeatureData[Sequence]" \
+    --input-path ${GG}gg_13_8_otus/rep_set/99_otus.fasta \
     --output-path ${OUT}99_otus.qza
 
 ### Then import the taxonomy file as a qiime2 object
-qiime tools import --type 'FeatureData[Taxonomy]' 
-    --input-format HeaderlessTSVTaxonomyFormat 
-    --input-path ${GG}gg_13_8_otus/taxonomy/99_otu_taxonomy.txt 
+qiime tools import --type 'FeatureData[Taxonomy]' \
+    --input-format HeaderlessTSVTaxonomyFormat \
+    --input-path ${GG}gg_13_8_otus/taxonomy/99_otu_taxonomy.txt \
     --output-path ${OUT}ref-99taxonomy.qza
 
 
-# TODO from here?    
+# TODO Do we need it?    
 ### Extracted the V4-V5 region by specifying the primers
-qiime feature-classifier 
-    extract-reads --i-sequences 99_otus.qza 
-    --p-f-primer CCAGCAGCYGCGGTAAN 
-    --p-r-primer CCGTCAATTNNTTTNANT 
-    --o-reads ref-99otus.qza
+#qiime feature-classifier 
+#    extract-reads --i-sequences 99_otus.qza 
+#    --p-f-primer CCAGCAGCYGCGGTAAN 
+#    --p-r-primer CCGTCAATTNNTTTNANT 
+#    --o-reads ref-99otus.qza
     
+
 ### Train the classifier
+qiime feature-classifier fit-classifier-naive-bayes \
+  --i-reference-reads ${OUT}99_otus.qza \
+  --i-reference-taxonomy ${OUT}ref-99taxonomy.qza \
+  --o-classifier ${OUT}classifier.qza 
 
-    qiime feature-classifier fit-classifier-naive-bayes
-  --i-reference-reads ref-99otus.qza 
-  --i-reference-taxonomy ref-99taxonomy.qza 
-  --o-classifier classifier.qza
 
-## Test the classifier
-We verify that the classifier works by classifying the representative sequences from Dobay et al.
+## Test the classifier : We verify that the classifier works by classifying the representative sequences from Dobay et al.
 
-qiime feature-classifier classify-sklearn 
-  --i-classifier classifier.qza 
-  --i-reads rep-seqs-dn-99.qza
-  --o-classification taxonomy.qza
+#qiime feature-classifier classify-sklearn 
+#  --i-classifier classifier.qza 
+#  --i-reads rep-seqs-dn-99.qza
+#  --o-classification taxonomy.qza
 
-qiime metadata tabulate \
-  --m-input-file taxonomy.qza \
-  --o-visualization taxonomy.qzv
+#qiime metadata tabulate \
+#  --m-input-file taxonomy.qza \
+#  --o-visualization taxonomy.qzv
+
--- src/pipeline_qiime2/qiime2_main.R
+++ src/pipeline_qiime2/qiime2_main.R
@@ -14,6 +14,7 @@ load(file=file.path(metadata_path, metadata.file))
 # you need to import your data into QIIME 2 manually by first creating a “manifest file” 
 #  and then using the qiime tools import command
 # https://docs.qiime2.org/2020.2/tutorials/importing/
+# https://drive.google.com/file/d/1dtl1XpvvNO015C2rIMEsNWm0u6cjQO6-/view?ts=5e625bda
 
 # 1 - create and save CSV manifest file
 # sample-id     forward-absolute-filepath       reverse-absolute-filepath
@@ -25,39 +26,54 @@ write_tsv(qiime.manifest, file.path(qiime_path, "manifest.tsv"))
 print(file.path(qiime_path, "manifest.tsv"))
 print(file.path(qiime_path, "twin_demux-paired-end.qza"))
 
-# 2 - run import script 
+
+##### Here we have to run qiime worklow by subsequently running 6 scripts
 # chmod +x 1_import.sh 2_vizualization.sh 3_denoise_feature_table.sh
-
-
+# 2 - run import script 
 # 3 - Denoising and feture table construction
-
-
 # 4 - OTU clustering
-
-### get data into R
-otus.99 <- qiime2R::read_qza("~/Projects_R/twins_microbiome_pipeline/data_set_twin/raw/qiime/table-dn-99.qza")
-otus.99$data
-# add sequence names?
-
 # 5 - tree
-
 # 6 - Taxonomy with greengenes (in dada2 is with SILVA) - is that a difference?
 
 
-### Build a phyloseq object
+
+############ Build a phyloseq object
 library(phyloseq)
-tree<-read_qza("~/QIIME2/mvpics/rooted-tree.qza")
+qiime.data.path <- "~/Projects_R/twins_microbiome_pipeline/data_set_twin/raw/qiime"
+
+# form meatadata
+qiime.metadata <- df.metadata
+names(qiime.metadata)[names(qiime.metadata)=="file"] <- "SampleID"
+
+# form feature table
+otus.99 <- qiime2R::read_qza(file.path(qiime.data.path, 'otu/table-dn-99.qza'))
+feature.table <- otus.99$data
+
+# tree
+tree <- qiime2R::read_qza(file.path(qiime.data.path, 'tree/rooted-tree.qza'))
+tree$data
+
+# taxonomy --- TODO - something wrong here. we have to run slassifier on data?
+tax.table <- qiime2R::read_qza(file.path(qiime.data.path, 'taxonomy/classifier.qza'))
+
+# TODO here - !
 
 taxonomy<-read_qza("~/QIIME2/mvpics/taxonomy.qza")
 tax_table<-do.call(rbind, strsplit(as.character(taxonomy$data$Taxon), "; "))
 colnames(tax_table)<-c("Kingdom","Phylum","Class","Order","Family","Genus","Species")
 rownames(tax_table)<-taxonomy$data$Feature.ID
 
-metadata<-read.table("~/QIIME2/mvpics/sample-metadata.tsv", sep='\t', header=T, row.names=1, comment="")
-metadata<-metadata[-1,]#remove the second line that specifies the data type
 
-physeq<-phyloseq(otu_table(otus$data, taxa_are_rows = T), phy_tree(tree$data), tax_table(tax_table), sample_data(metadata))
 
+
+physeq<-phyloseq(
+  otu_table(feature.table, taxa_are_rows = T), ### is that correct?
+  phy_tree(tree$data), 
+  tax_table(tax_table), 
+  sample_data(metadata)
+  )
+
+# save it
 
 
 
